0df399d3ee66773567f810740bde812ff9f52c54
akaufhold <info@stackfactory.de>
2024-11-25 20:38:57 +0100

    feat: import fa js

--- build/assets/js/home.js
+++ build/assets/js/home.js
@@ -2,3 +2,9 @@
 
 // IMPORT SCSS
 import '../scss/home.scss'
+
+import '@fortawesome/fontawesome-free/js/fontawesome.js'
+
+import '@fortawesome/fontawesome-free/js/solid.js'
+import '@fortawesome/fontawesome-free/js/brands.js'
+import '@fortawesome/fontawesome-free/js/regular.js'
