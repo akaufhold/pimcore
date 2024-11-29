de0b79b356d5b54535b23d23b80b53a0957119dd
akaufhold <info@stackfactory.de>
2024-11-29 14:32:14 +0100

    feat: add filter for listings for content controller

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -14,8 +14,18 @@ use Pimcore\Model\Asset;
 use Pimcore\Model\Asset\Listing as AssetListing;
 use Pimcore\Model\Asset\Image;
 
+use App\Service\ContentService;
+
 class ContentController extends FrontendController
 {
+
+    private ContentService $contentService;
+
+    public function __construct(ContentService $contentService)
+    {
+        $this->contentService = $contentService;
+    }
+
     /**
      * Action for content template
      * Request $request
@@ -24,22 +34,33 @@ class ContentController extends FrontendController
      */
     public function templateAction(Request $request): Response
     {
+        $document = $this->document;
+        $templateFile = $document->getTemplate() ?? 'content/home.html.twig';
+
         $logo = $this->getAssetImageById(13);
 		$socialRoot = $this->getDocumentById(3);
-        $socialChildren = $this->getChildrensByPid($socialRoot);
+        $socialChildren = $this->getChildrenListingByPid($socialRoot);
 		$mainNavRoot = $this->getDocumentById(2);
-        $mainNavChildren = $this->getChildrensByPid($mainNavRoot);
+        $mainNavChildren = $this->getChildrenListingByPid($mainNavRoot);
+        $mainNavChildrenFiltered = $this->filterListingWithBool($mainNavChildren, 'main_nav_hide', 0);
 
         $carouselItems = $this->getAssetListingByPid(2);
 
-        return $this->render('content/home.html.twig', [
+        $renderParams = [
             'logo' => $logo,
             'socialRoot' => $socialRoot,
             'socialChildren' => $socialChildren,
             'carouselItems' => $carouselItems,
             'mainNavRoot' => $mainNavRoot,
-            'mainNavChildren' => $mainNavChildren,
-        ]);
+            'mainNavChildren' => $mainNavChildrenFiltered,
+        ];
+        $additionalContent = $this->contentService->getAdditionalContent($document);
+
+        if ($additionalContent) {
+            $renderParams['additionalContent'] = $additionalContent;
+        }
+
+        return $this->render($templateFile, $renderParams);
     }
 
     /**
@@ -59,9 +80,28 @@ class ContentController extends FrontendController
      * 
      * @return Listing
      */
-    public function getChildrensByPid(Page $page): Listing 
+    public function getChildrenListingByPid(Page $page): Listing 
     {
         return $socialPages = $page ? $page->getChildren() : [];
+    }
+
+    /**
+     * Filter Navigation Elements with property
+     * Listing $listing
+     * String $propertyToFilter
+     * Boolean $filterValue
+     * 
+     * @return Listing
+     */
+    public function filterListingWithBool(Listing $listing, string $propertyToFilter, bool $filterValue): Listing 
+    {
+        $filteredListing = new Listing();
+        $filteredListing->setCondition('id IN (?)', [
+            array_map(fn($doc) => $doc->getId(), 
+                array_filter($listing->getDocuments(), fn($doc) => (bool) $doc->getProperty($propertyToFilter, false) === $filterValue)
+            )
+        ]);
+        return $filteredListing;
     }
 
     /**
