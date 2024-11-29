26f376d2e05dbcb99b2bf612b71f5ecb8b1882fe
akaufhold <info@stackfactory.de>
2024-11-29 20:32:59 +0100

    feat: add service class for waste calculator

--- src/Service/WasteCalculatorService.php
+++ src/Service/WasteCalculatorService.php
@@ -1,27 +1,31 @@
 <?php
 
 namespace App\Service;
- 
+use Pimcore\Model\DataObject\PersonsHousehold;
+
 class WasteCalculatorService
 {
     public function calculateWasteData(PersonsHousehold $household): array
     {
         $data = [];
-        foreach ($household->getMinimumContainerPickup() as $pickup) {
-            $container = $pickup->getContainerSize();
-            $minimumVolume = $pickup->getMinimumPickups() * $container->getVolumen();
+
+        foreach ($household->getMinimumContainerPickup() as $minPickupEntry) {     
+            $container = $minPickupEntry->getContainerSize()[0];
+
+            $minimumVolume = $minPickupEntry->getMinimumContainerPickup() * $container->getVolume();
             $data[] = [
-                'containerSize' => $container->getVolumen(),
+                'containerSize' => $container->getVolume(),
                 'minimumVolume' => $minimumVolume,
-                'pickups' => $pickup->getMinimumPickups(),
-                'fee' => $this->calculateFee($container->getVolumen(), $pickup->getMinimumPickups())
+                'pickups' => $minPickupEntry->getMinimumContainerPickup(),
+                'fee' => $this->calculateFee($container->getVolume(), $minPickupEntry->getMinimumContainerPickup())
             ];
         }
+
         return $data;
     }
 
     private function calculateFee(float $volume, int $pickups): float
     {
-        return $volume * $pickups * 0.13; // Beispielgebühr je Liter
+        return $volume * $pickups * 0.13;
     }
 }
