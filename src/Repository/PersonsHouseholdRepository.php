11096d8a2a182322810a8734ef696358f799f26a
akaufhold <info@stackfactory.de>
2024-11-29 20:32:23 +0100

    feat: add household repo

---
+++ src/Repository/PersonsHouseholdRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use Pimcore\Model\DataObject\PersonsHousehold;
+
+class PersonsHouseholdRepository
+{
+    public function __constructor () {
+
+    }
+
+    public function findAll(): array
+    {
+        $listing = new PersonsHousehold\Listing();
+        $listing->setUnpublished(false);
+        return $listing->load();
+    }
+
+    public function findById(int $id): ?PersonsHousehold
+    {
+        return PersonsHousehold::getById($id);
+    }
+}
