07743cc7537d698d82a478e638ed927021e43a1f
akaufhold <info@stackfactory.de>
2024-12-03 13:24:27 +0100

    feat: transfer filter to listing service

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -11,24 +11,29 @@ use Pimcore\Model\Document;
 use Pimcore\Model\Document\Page;
 use Pimcore\Model\Document\Listing;
 use Pimcore\Model\Asset;
-use Pimcore\Model\Asset\Listing as AssetListing;
 use Pimcore\Model\Asset\Image;
 
 use App\Service\ContentService;
+use App\Service\AssetService;
+use App\Service\ListingService;
 
 class ContentController extends FrontendController
 {
 
     private ContentService $contentService;
+    private AssetService $assetService;
+    private ListingService $listingService;
 
-    public function __construct(ContentService $contentService)
+    public function __construct(ContentService $contentService, AssetService $assetService, ListingService $listingService)
     {
         $this->contentService = $contentService;
+        $this->assetService = $assetService;
+        $this->listingService = $listingService;
     }
 
     /**
      * Action for content template
-     * Request $request
+     * @param Request $request
      * 
      * @return Response
      */
@@ -42,9 +47,9 @@ class ContentController extends FrontendController
         $socialChildren = $this->getChildrenListingByPid($socialRoot);
 		$mainNavRoot = Document::getById(2);
         $mainNavChildren = $this->getChildrenListingByPid($mainNavRoot);
-        $mainNavChildrenFiltered = $this->filterListingWithBool($mainNavChildren, 'main_nav_hide', 0);
+        $mainNavChildrenFiltered = $this->listingService->filterListingWithBool($mainNavChildren, 'main_nav_hide', 0);
 
-        $carouselItems = $this->getAssetListingByPid(2);
+        $carouselItems = $this->assetService->getAssetListingByPid(2);
 
         $renderParams = [
             'logo' => $logo,
@@ -55,7 +60,7 @@ class ContentController extends FrontendController
             'mainNavChildren' => $mainNavChildrenFiltered,
         ];
 
-        [$renderParams['allHouseholds'], $renderParams['currentHouseholdCalc']] = $this->contentService->getAdditionalContent($document, $request);
+        [$renderParams['allData'], $renderParams['curData'], $renderParams['curPersonId']] = $this->contentService->getAdditionalContent($document, $request);
 
         return $this->render($templateFile, $renderParams);
     }
@@ -69,38 +74,5 @@ class ContentController extends FrontendController
     public function getChildrenListingByPid(Page $page): Listing 
     {
         return $socialPages = $page ? $page->getChildren() : [];
-    }
-
-    /**
-     * Filter Navigation Elements with property
-     * Listing $listing
-     * String $propertyToFilter
-     * Boolean $filterValue
-     * 
-     * @return Listing
-     */
-    public function filterListingWithBool(Listing $listing, string $propertyToFilter, bool $filterValue): Listing 
-    {
-        $filteredListing = new Listing();
-        $filteredListing->setCondition('id IN (?)', [
-            array_map(fn($doc) => $doc->getId(), 
-                array_filter($listing->getDocuments(), fn($doc) => (bool) $doc->getProperty($propertyToFilter, false) === $filterValue)
-            )
-        ]);
-        return $filteredListing;
-    }
-
-    /**
-     * Get Asset Items from Pid
-     * 
-     * @param int $pageId
-     * @return array
-     */
-    private function getAssetListingByPid(int $pageId): array 
-    {
-        $list = new AssetListing();
-        $list->setCondition('parentId = ?', [$pageId]);
-        $assetList = $list->load();
-        return $assetList;
     }
 }
---
+++ src/Service/ListingService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\PersonsHouseholdRepository;
+use App\Service\WasteCalculatorService;
+use Pimcore\Model\Document;
+use Pimcore\Model\Document\Listing;
+
+class ListingService
+{
+
+/**
+     * Filter Navigation Elements with property
+     * @param Listing $listing
+     * @param String $propertyToFilter
+     * @param Boolean $filterValue
+     * 
+     * @return Listing
+     */
+    public function filterListingWithBool(Listing $listing, string $propertyToFilter, bool $filterValue): Listing 
+    {
+        $filteredListing = new Listing();
+        $filteredListing->setCondition('id IN (?)', [
+            array_map(fn($doc) => $doc->getId(), 
+                array_filter($listing->getDocuments(), fn($doc) => (bool) $doc->getProperty($propertyToFilter, false) === $filterValue)
+            )
+        ]);
+        return $filteredListing;
+    }
+}
