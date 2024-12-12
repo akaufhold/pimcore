a8aa84c858a4fb5f2fe4ae16ec4429a83f4459a1
akaufhold <info@stackfactory.de>
2024-12-12 20:54:40 +0100

    feat: adjust burger menu icon animation

--- build/assets/js/navigation.js
+++ build/assets/js/navigation.js
@@ -3,7 +3,6 @@ const bmClassName = 'burger-menu'
 window.addEventListener('load', (event) => {
   const mainNavigation = document.getElementsByClassName('main-navigation')[0]
   document.getElementsByClassName(bmClassName)[0].addEventListener('click', function (e) {
-    console.log(e)
     mainNavigation.classList.toggle('opened')
     e.target.classList.toggle('opened')
   })
