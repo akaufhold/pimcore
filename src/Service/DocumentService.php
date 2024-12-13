a3abb3b379b8be33ebfbd41b9e78896e6e68a4eb
akaufhold <info@stackfactory.de>
2024-12-13 12:41:35 +0100

    feat: transfer function for getting root child docs to DocumentService

--- src/Service/DocumentService.php
+++ src/Service/DocumentService.php
@@ -7,27 +7,39 @@ use App\Service\WasteCalculatorService;
 use Pimcore\Model\Document;
 use Pimcore\Model\Document\Listing;
 
+use Pimcore\Model\Exception\NotFoundException;
+
 class DocumentService
 {
-	/**
-	 * Filter Navigation Elements with property
-	 * @param integer $docId
-	 * @param string $propertyName
-	 * 
-	 * @return string
-	 */
-	public function getPropFromDoc($docId, $propertyName) :string
-	{
-		$document = Document::getById($docId); 
-		if (!$document) {
-			throw new NotFoundException("Document with ID {$docId} not found.");
-		}
-		$propertyValue = $document->getProperty($propertyName); 
-		if (!$propertyValue) {
-			throw new NotFoundException("Property {$propertyName} not found.");
-			return '';
-		}
-		return $propertyValue;
-	}
+    /**
+     * Filter Navigation Elements with property
+     * @param integer $docId
+     * @param string $propertyName
+     * 
+     * @return string
+     */
+    public function getPropFromDoc($docId, $propertyName) :string
+    {
+        $document = Document::getById($docId); 
+        if (!$document) {
+                throw new NotFoundException("Document with ID {$docId} not found.");
+        }
+        $propertyValue = $document->getProperty($propertyName); 
+        if (!$propertyValue) {
+                throw new NotFoundException("Property {$propertyName} not found.");
+                return '';
+        }
+        return $propertyValue;
+    }
 
+    /**
+     * Get childrens from navigation root 
+     * Page $page
+     * 
+     * @return Listing
+     */
+    public function getChildrenListingByPid(Page $root): Listing 
+    {
+        return $socialPages = $root ? $root->getChildren() : [];
+    }
 }
