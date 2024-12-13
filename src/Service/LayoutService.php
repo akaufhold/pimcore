5dcee2741ef2fcacf64858f263a91dbcf56a76d4
akaufhold <info@stackfactory.de>
2024-12-13 13:01:09 +0100

    refactor: improve structure and modularize rendering parameters

--- src/Service/LayoutService.php
+++ src/Service/LayoutService.php
@@ -3,31 +3,40 @@
 namespace App\Service;
 
 use Pimcore\Model\Document;
-use App\Service\DocumentService;
+use Pimcore\Model\Asset;
 
-class layoutService
+use App\Service\DocumentService;
+use App\Service\ListingService;
+
+class LayoutService
 {
 		private DocumentService $documentService;
+    private ListingService $listingService;
+
+    private $pageRootId = ROOT_ID;
 
     public function __construct(
 				DocumentService $documentService,
+				ListingService $listingService,
     ) {
 				$this->documentService = $documentService;
+				$this->listingService = $listingService;
     }
 
 		/**
-		 * Get common rendering parameters for layout
+		 * Get common rendering parameters for default layout
 		 * 
 		 * @return array
 		 */
     public function getRenderParams() :array
 		{
+			$mainNavRoot = Document::getById((int)($this->documentService->getPropFromDoc($this->pageRootId, 'mainNavId')));
+			$socialRoot = Document::getById((int)($this->documentService->getPropFromDoc($this->pageRootId, 'socialNavId')));
+
 			$renderParams['logo'] = Asset::getById((int)($this->documentService->getPropFromDoc($this->pageRootId, 'logoId')));
-			$renderParams['socialRoot'] = Document::getById((int)($this->documentService->getPropFromDoc($this->pageRootId, 'socialNavId')));
-			$renderParams['socialChildren'] = $this->getChildrenListingByPid($socialRoot);
-			$renderParams['mainNavRoot'] = Document::getById((int)($this->documentService->getPropFromDoc($this->pageRootId, 'mainNavId')));
-			$renderParams['mainNavChildren'] = $this->getChildrenListingByPid($mainNavRoot);
-			$renderParams['mainNavChildrenFiltered'] = $this->listingService->filterListingWithBool($mainNavChildren, 'main_nav_hide', 0);
+			$renderParams['socialNavItems'] = $this->documentService->getChildrenListingByPid($socialRoot);
+			$renderParams['mainNavItems'] = $this->documentService->getChildrenListingByPid($mainNavRoot);
+			$renderParams['mainNavItemsFiltered'] = $this->listingService->filterListingWithBool($mainNavChildren, 'main_nav_hide', 0);
 			$renderParams['sliderItems'] = $this->assetService->getAssetListingByPid($this->documentService->getPropFromDoc($this->pageRootId, 'sliderAssetsRoot'));
 
 			return $renderParams;
